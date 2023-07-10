68cb809b26a7a68c26c7762afaa53a9f7436d705
dongjun <yh02195@gmail.com>
2023-07-10 19:59:29 +0900

    시간초과 해결

--- 박동준/Baekjoon/Fly_me_to_the_Alpha_Centauri_1011.java
+++ 박동준/Baekjoon/Fly_me_to_the_Alpha_Centauri_1011.java
@@ -9,45 +9,27 @@ public class Fly_me_to_the_Alpha_Centauri_1011 {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            int cnt = 0;
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int distance = n2-n1;
 
-            while (start < end) {
+            long answer = 1;
+            long start = 1;
+            long end = 1;
+            long cnt = 1;
+
+            while (distance > start + end) {
                 start += ++cnt;
-                end -= cnt;
+                end += cnt;
+                answer ++;
             }
 
-            System.out.println(start >= end + cnt ? 2 * cnt - 1 : 2 * cnt);
+            if (distance <= start + end - cnt) {
+                System.out.println(answer * 2 - 1);
+                continue;
+            }
+
+            System.out.println(answer*2);
         }
     }
-
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//
-//        for (int i = 0; i < n; i++) {
-//            int n1 = sc.nextInt();
-//            int n2 = sc.nextInt();
-//            int distance = n2-n1;
-//
-//            int answer = 1;
-//            int start = 1;
-//            int end = 1;
-//            int cnt = 1;
-//
-//            while (distance > start + end) {
-//                start += ++cnt;
-//                end += ++cnt;
-//                answer ++;
-//            }
-//
-//            if (distance <= start + end - cnt) {
-//                System.out.println(answer * 2 - 1);
-//                continue;
-//            }
-//
-//            System.out.println(answer*2);
-//        }
-//    }
 }
